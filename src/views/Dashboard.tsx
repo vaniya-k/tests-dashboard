c98e3b7b678e8f42bdc00fd64369a9000eda2499
vaniya <vaniya.karakasiyan@gmail.com>
2021-01-25 02:36:39 +0300

    applied filtering logic

--- src/views/Dashboard.tsx
+++ src/views/Dashboard.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import TestsTable from '../components/TestsTable'
 import { fetchJSONHook }  from '../utils/hooks'
 import { SITES_API, TESTS_API } from '../utils/constants'
@@ -8,21 +8,32 @@ import { getSiteString } from '../utils/format-tools'
 const Dashboard: DashboardViewType = () => {
   const [items, itemsRequestCycle] = fetchJSONHook(`${TESTS_API}`)
   const [sites, sitesRequestCycle] = fetchJSONHook(`${SITES_API}`)
+  const [filterQuery, setFilterQuery] = useState('')
 
   let itemsToRender: TestsTableRowsData = []
 
   if(items && sites) {
-    itemsToRender = (items as TestsType).map((item: TestType) => {
-      return {...item, site: getSiteString(item, (sites as SitesType))}
-    })
+    itemsToRender =
+      (items as TestsType)
+        .filter((item: TestType) => item.name.toLowerCase().includes(filterQuery.toLowerCase()))
+        .map((item: TestType) => {return {...item, site: getSiteString(item, (sites as SitesType))}})
   }
 
   return (
     <>
       <header className="page-container page-header">
         <div className="page-header__search-wrapper">
-          <input type="text" className="page-header__search-input" />
-          <span className="page-header__search-results">7 tests</span>
+          <img
+            className="page-header__search-icon"
+            src="./assets/icons/magnifying-glass.svg"
+          />
+          <input type="text" className="page-header__search-input" onChange={(e) => setFilterQuery(e.target.value)}/>
+          {
+            items &&
+            <span className="page-header__search-results">
+              {itemsToRender.length === 1 ? '1 result' : `${itemsToRender.length} results`}
+            </span>
+          }
         </div>
       </header>
       <main className="page-container page-main">
