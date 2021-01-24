2e5e97697b0951906c3d5c5387811f328ffc2a0d
vaniya <vaniya.karakasiyan@gmail.com>
2021-01-24 15:31:47 +0300

    added eslint and prettier to the initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,14 @@
 import React from 'react'
 import Dashboard from './views/Dashboard'
 import Results from './views/Results'
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 const App = () => {
   return (
     <Router>
       <Switch>
-        <Route
-          exact path="/"
-          render={() => <Dashboard/>}
-        />
-        <Route
-          exact path="/results/:testId"
-          render={(props) => <Results id={props.match.params.testId}/>}
-        />
+        <Route exact path='/' render={() => <Dashboard />} />
+        <Route exact path='/results/:testId' render={(props) => <Results id={props.match.params.testId} />} />
       </Switch>
     </Router>
   )
--- src/views/Results.tsx
+++ src/views/Results.tsx
@@ -1,9 +1,7 @@
 import React from 'react'
 
-const Results = ({id}) => {
-  return (
-    <div>Results {`${id}`}</div>
-  )
+const Results = ({ id }) => {
+  return <div>Results {`${id}`}</div>
 }
 
 export default Results
